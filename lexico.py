af88916f2e592959b9d6ef69a8a23afd9a2a725c
Eduardomsj-cyber <138149064+Eduardomsj-cyber@users.noreply.github.com>
2025-04-04 11:09:55 -0600

    Add files via upload

---
+++ lexico.py
@@ -0,0 +1,38 @@
+# lexer_demo.py
+import re
+
+# Expresiones regulares para tokens básicos
+TOKEN_SPEC = [
+    ('PALABRA_CLAVE', r'\bdef\b|\breturn\b'),
+    ('IDENTIFICADOR', r'\b[a-zA-Z_][a-zA-Z0-9_]*\b'),
+    ('NUMERO', r'\b\d+\b'),
+    ('OPERADOR', r'[+\-*/=]'),
+    ('DELIMITADOR', r'[(),:]'),
+    ('COMENTARIO', r'#.*'),
+    ('ESPACIO', r'[ \t\n]+'),
+    ('ERROR', r'.'),  # Cualquier otro símbolo
+]
+
+def tokenize(code):
+    tokens = []
+    regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in TOKEN_SPEC)
+    for match in re.finditer(regex, code):
+        tipo = match.lastgroup
+        valor = match.group()
+        if tipo == 'ESPACIO':
+            continue
+        elif tipo == 'ERROR':
+            raise SyntaxError(f"🚨 Error léxico pupu: carácter no válido '{valor}'")
+        tokens.append((tipo, valor))
+    return tokens
+
+# error aqui esta
+codigo = "a + $b"
+
+try:
+    resultado = tokenize(codigo)
+    print("Tokens generados:")
+    for token in resultado:
+        print(token)
+except SyntaxError as e:
+    print(e)
